f636f14f4a3d01ce90539bb89e7c55b706c51f07
Hojer Filip (256737) <256737@vutbr.cz>
2025-04-10 14:03:52 +0200

    pwm komponenta + simulace

---
+++ pwm/pwm.srcs/sim_1/new/tb_pwm.vhd
@@ -0,0 +1,78 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Thu, 10 Apr 2025 11:37:48 GMT
+-- Request id : cfwk-fed377c2-67f7ad8c9c188
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+entity tb_pwm is
+end tb_pwm;
+
+architecture tb of tb_pwm is
+   constant max_count_bits : positive:= 21;
+    component pwm
+        port (en      : in std_logic;
+              rst     : in std_logic;
+              clk     : in std_logic;
+              duty_in : in unsigned (max_count_bits -1 downto 0);
+              pwm_out : out std_logic);
+    end component;
+
+    signal en      : std_logic;
+    signal rst     : std_logic;
+    signal clk     : std_logic;
+    signal duty_in : unsigned (max_count_bits -1 downto 0);
+    signal pwm_out : std_logic;
+
+    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : pwm
+    port map (en      => en,
+              rst     => rst,
+              clk     => clk,
+              duty_in => duty_in,
+              pwm_out => pwm_out);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- ***EDIT*** Adapt initialization as needed
+        en <= '0';
+        duty_in <= (others => '0');
+
+        -- Reset generation
+        -- ***EDIT*** Check that rst is really your reset signal
+        rst <= '1';
+        wait for 100 ns;
+        rst <= '0';
+        wait for 100 ns;
+        
+        en <= '1';
+        -- ***EDIT*** Add stimuli here
+        duty_in <= "000011001100110011010";
+        wait for 100ms;
+        
+        
+        duty_in <= "000011011111110011010";
+        wait for 100ms;
+        
+        
+        
+        duty_in <= "000110011001100110011";
+        wait for 100ms;
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
--- pwm/pwm.srcs/sources_1/new/pwm.vhd
+++ pwm/pwm.srcs/sources_1/new/pwm.vhd
@@ -1,18 +1,19 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 
 entity pwm is
-    generic( position_bits : positive := 8);
+    generic(max_count_bits : positive := 21);
     Port ( en : in STD_LOGIC;
            rst : in STD_LOGIC;
            clk : in STD_LOGIC;
-           pwm_in : in STD_LOGIC_vector(position_bits -1 downto 0);
+           duty_in : in unsigned(max_count_bits -1 downto 0);
            pwm_out : out STD_LOGIC);
 end pwm;
 
 architecture Behavioral of pwm is
-signal sig_count : std_logic_vector(position_bits -1 downto 0);
+signal sig_count : unsigned(max_count_bits -1 downto 0);
 signal sig_pwm_out : std_logic;
 begin
 
@@ -24,14 +25,15 @@ if(rising_edge(clk)) then
         sig_pwm_out <= '0';
         sig_count <=  (others => '0');
     elsif(en='1') then
-        if(
-
-
-
-
+        sig_count <= sig_count + 1;
+        sig_pwm_out <= '0';
+        if sig_count < duty_in then
+            sig_pwm_out <= '1';
+        end if;
     end if;
 end if;
 
+pwm_out <= sig_pwm_out;
 end process;
 
 end Behavioral;
